850a5dc3466f2926f564329726b0ccf623c106a1
Mantasha Siddiki <siddikimantasha644@gmail.com>
2025-08-29 22:31:14 +0530

    first commit

--- client/src/apiscalls/axiosConfig.js
+++ client/src/apiscalls/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://filmi-dhamaka.vercel.app', // backend URL
+  baseURL: 'https://filmidhamaka.onrender.com/', // backend URL
   headers: {
     'Content-Type': 'application/json',
   }
